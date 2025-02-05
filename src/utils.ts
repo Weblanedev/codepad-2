2c22f1c290b643114cf438d6892f8f79ee720d98
Bunmi Oye <bunmioye09@gmail.com>
2025-02-05 15:53:36 +0100

    update currency to naira

--- src/utils.ts
+++ src/utils.ts
@@ -1,29 +1,29 @@
-import lean from "./assets/the lean stup.webp";
-import influence from "./assets/influence.png";
-import hooked from "./assets/Hooked-updated-cover-US.webp";
-import phoenix from "./assets/the phoenix project.jpeg";
-import digital from "./assets/Digital-Marketing-for-Dummies-book-cover.jpg";
+import lean from './assets/the lean stup.webp';
+import influence from './assets/influence.png';
+import hooked from './assets/Hooked-updated-cover-US.webp';
+import phoenix from './assets/the phoenix project.jpeg';
+import digital from './assets/Digital-Marketing-for-Dummies-book-cover.jpg';
 
 export const genres = [
   {
     id: 1,
-    name: "Business/Entrepreneurship",
+    name: 'Business/Entrepreneurship',
   },
   {
     id: 2,
-    name: "Psychology/Sales",
+    name: 'Psychology/Sales',
   },
   {
     id: 3,
-    name: "Business/Marketing",
+    name: 'Business/Marketing',
   },
   {
     id: 4,
-    name: "Business/Technology",
+    name: 'Business/Technology',
   },
   {
     id: 5,
-    name: "Marketing/Technology",
+    name: 'Marketing/Technology',
   },
 ];
 
@@ -42,56 +42,56 @@ export const books = [
     id: 1,
     title:
       "The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses",
-    author: "Eric Ries",
-    genre: "Business/Entrepreneurship",
+    author: 'Eric Ries',
+    genre: 'Business/Entrepreneurship',
     img: lean,
-    price: "$65.99",
+    price: '₦6,599.99',
     summary:
-      "This book presents a methodology for developing businesses and products based on the concept of validated learning, experimentation, and iterative product releases.",
+      'This book presents a methodology for developing businesses and products based on the concept of validated learning, experimentation, and iterative product releases.',
   },
   {
     id: 2,
-    title: "Influence: The Psychology of Persuasion",
-    author: "Robert B. Cialdini",
-    genre: "Psychology/Sales",
+    title: 'Influence: The Psychology of Persuasion',
+    author: 'Robert B. Cialdini',
+    genre: 'Psychology/Sales',
     img: influence,
 
-    price: "$72.50",
+    price: '₦7,499.99',
     summary:
       "This book explores the psychology behind why people say 'yes' and how to apply these understandings in marketing, sales, and everyday life.",
   },
   {
     id: 3,
-    title: "Hooked: How to Build Habit-Forming Products",
-    author: "Nir Eyal",
-    genre: "Business/Marketing",
+    title: 'Hooked: How to Build Habit-Forming Products',
+    author: 'Nir Eyal',
+    genre: 'Business/Marketing',
     img: hooked,
 
-    price: "$80.99",
+    price: '₦13,999.99',
     summary:
       "This book provides insights into the psychology of habit-forming products and offers practical advice for creating products that users can't put down.",
   },
   {
     id: 4,
     title:
-      "The Phoenix Project: A Novel About IT, DevOps, and Helping Your Business Win",
-    author: "Gene Kim, Kevin Behr, George Spafford",
-    genre: "Business/Technology",
+      'The Phoenix Project: A Novel About IT, DevOps, and Helping Your Business Win',
+    author: 'Gene Kim, Kevin Behr, George Spafford',
+    genre: 'Business/Technology',
     img: phoenix,
 
-    price: "$97.25",
+    price: '₦9,725.00',
     summary:
       "This novel tells the story of an IT manager's journey to save his company's failing IT infrastructure by applying principles of DevOps.",
   },
   {
     id: 5,
-    title: "Digital Marketing for Dummies",
-    author: "Ryan Deiss, Russ Henneberry",
-    genre: "Marketing/Technology",
+    title: 'Digital Marketing for Dummies',
+    author: 'Ryan Deiss, Russ Henneberry',
+    genre: 'Marketing/Technology',
     img: digital,
 
-    price: "$84.99",
+    price: '₦8,499.99',
     summary:
-      "This book offers comprehensive guidance on digital marketing strategies, including SEO, content marketing, social media, email marketing, and more.",
+      'This book offers comprehensive guidance on digital marketing strategies, including SEO, content marketing, social media, email marketing, and more.',
   },
 ];
